9a9bd1fbc4e47d852151ef4b8424953a879c031b
AbrahaKahsay <abrahak81@gmail.com>
2023-08-05 01:08:14 +0300

    Populate seed file

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,25 @@
 #
 #   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
 #   Character.create(name: "Luke", movie: movies.first)
+
+
+# clear existing data 
+Event.:destroy_all
+
+events = Event.create([
+    {
+        title: "Jane's Graduation",
+        location: "Jane's house",
+        date: DateTime.new(2023, 8, 10, 12, 0)
+    },
+    {
+        title: "Music Festival",
+        location: "Central perk",
+        date: DateTime.new(2023, 10, 10, 8, 0)
+    },
+    {
+        title: "Football game",
+        location: "Local stadium",
+        date: DateTime.new(2023, 9, 8, 10, 0)
+    }
+])
