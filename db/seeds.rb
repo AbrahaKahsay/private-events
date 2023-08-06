81f72cfa207f385b7f5cb761b9315053eecd588d
AbrahaKahsay <abrahak81@gmail.com>
2023-08-06 06:23:11 +0300

    Modify seed

--- db/seeds.rb
+++ db/seeds.rb
@@ -8,22 +8,35 @@
 
 
 # clear existing data 
+# Event.destroy_all
+
+# events = Event.create([
+#     {
+#         title: "Jane's Graduation",
+#         location: "Jane's house",
+#         date: DateTime.new(2023, 8, 10, 12, 0)
+#     },
+#     {
+#         title: "Music Festival",
+#         location: "Central perk",
+#         date: DateTime.new(2023, 10, 10, 8, 30)
+#     },
+#     {
+#         title: "Football game",
+#         location: "Local stadium",
+#         date: DateTime.new(2023, 9, 8, 10, 0)
+#     }
+# ])
+
+
+User.destroy_all
 Event.destroy_all
 
-events = Event.create([
-    {
-        title: "Jane's Graduation",
-        location: "Jane's house",
-        date: DateTime.new(2023, 8, 10, 12, 0)
-    },
-    {
-        title: "Music Festival",
-        location: "Central perk",
-        date: DateTime.new(2023, 10, 10, 8, 30)
-    },
-    {
-        title: "Football game",
-        location: "Local stadium",
-        date: DateTime.new(2023, 9, 8, 10, 0)
-    }
-])
+# Create Creators (Users)
+creator1 = User.create(name: 'John Doe', email: 'john@example.com', password: 'password')
+creator2 = User.create(name: 'Jane Smith', email: 'jane@example.com', password: 'password')
+
+# Create Events and associate them with the creators
+event1 = creator1.created_events.create(title: 'Summer Picnic', location: 'Central Park', date: DateTime.new(2023, 8, 10, 12, 0))
+event2 = creator1.created_events.create(title: 'Music Festival', location: 'Local Stadium', date: DateTime.new(2023, 9, 5, 18, 30))
+event3 = creator2.created_events.create(title: 'Board Games Night', location: 'Community Center', date: DateTime.new(2023, 8, 20, 19, 0))
