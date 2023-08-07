d8a2ff6a5deb4cf209e3ec6dc344703aac35a653
AbrahaKahsay <abrahak81@gmail.com>
2023-08-07 23:03:08 +0300

    Modify event controller for create method

--- app/controllers/events_controller.rb
+++ app/controllers/events_controller.rb
@@ -3,4 +3,20 @@ class EventsController < ApplicationController
     def index
         @events = Event.all
     end
+
+    def new
+        @event = current_user.created_events.build
+    end      
+    
+    def create
+        @event = current_user.created_events.build(event_params)
+      
+        if @event.save
+          redirect_to @event, notice: 'Event was successfully created.'
+        else
+          render :new
+        end
+    end
+      
+    
 end
